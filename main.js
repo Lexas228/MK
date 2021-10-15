a655a231e512d4f0297f8b23b1d2f77d93c669b4
Dmityi Shishlov <dima.shishlov89@mail.ru>
2021-10-15 23:47:05 +0300

    I made something really imagine!

--- main.js
+++ main.js
@@ -1,47 +1,96 @@
+let $arena = document.querySelector('.arenas');
 let player1 = {
+    player: 1,
     name: 'Scorpion',
     hp:100,
     img:"http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
-    weapon:["knife", "gun", "pick"],
+    weapon: ["knife", "gun", "pick"],
     attack: function (){
-        alert(this.name + "fight...")
+        hit(player2, this.weapon[Math.floor(Math.random() * this.weapon.length)]);
     }
 };
 
 let player2 = {
+    player: 2,
     name: 'Sonya',
-    hp:99,
+    hp: 100,
     img:"http://reactmarathon-api.herokuapp.com/assets/sonya.gif",
-    weapon:["arrow", "brain"],
+    weapon: ["arrow", "brain"],
     attack: function (){
-        alert(this.name + "fight...")
+        hit(player1, this.weapon[Math.floor(Math.random() * this.weapon.length)]);
     }
 };
 
-function createPlayer(className, player){
-    let $div = document.createElement('div');
-    $div.classList.add(className)
-    let $divProgress = document.createElement('div');
-    $divProgress.classList.add('progressbar');
-    let $divCharacter = document.createElement('div');
-    $divCharacter.classList.add('character');
-    $div.appendChild($divProgress);
-    $div.appendChild($divCharacter);
-    let $divName = document.createElement('div');
-    $divName.classList.add('name');
-    $divName.innerText = player.name;
-    let $divLife = document.createElement('div');
-    $divLife.classList.add('life');
-    $divLife.innerText = player.hp;
-    $divLife.style.width = '100%';
-    $divProgress.appendChild($divName);
-    $divProgress.appendChild($divLife);
-    let $img = document.createElement("img");
-    $img.src = player.img;
-    $divCharacter.appendChild($img);
-    let $arena = document.querySelector('.arenas');
-    $arena.appendChild($div);
+
+function createElementWithClassName(tag, className) {
+    let $div = document.createElement(tag);
+    if (className) {
+       $div.classList.add(className);
+    }
+    return $div;
 }
 
-createPlayer('player1', player1);
-createPlayer('player2', player2)
+function createPlayer(player){
+    let $div = createElementWithClassName('div','player' + player.player);
+    let $divProgress = createElementWithClassName('div','progressbar')
+    let $divCharacter = createElementWithClassName('div','character')
+    $div.appendChild($divProgress);
+    $div.appendChild($divCharacter);
+    let $divName = createElementWithClassName('div', 'name')
+    $divName.innerText = player.name;
+    let $divLife = createElementWithClassName('div', 'life')
+    $divLife.style.width = player.hp + '%';
+    $divProgress.appendChild($divName);
+    $divProgress.appendChild($divLife);
+    let $img = createElementWithClassName('img')
+    $img.src = player.img;
+    $divCharacter.appendChild($img);
+    return $div;
+}
+function getRandomDamage(maxDamage){
+    return Math.ceil(Math.random() * maxDamage);
+}
+function hit(player, weapon){
+    player.hp -= getRandomDamage(mapOfDamage.get(weapon));
+    if(player.hp < 0) player.hp = 0;
+}
+
+function playerWin(player){
+    const $winTitle = createElementWithClassName('div', 'winTitle');
+    $winTitle.innerText = player.name + ' win!';
+    return $winTitle;
+}
+
+const $divPlayer1 = createPlayer(player1);
+const $divPlayer2 = createPlayer(player2);
+$arena.appendChild($divPlayer1);
+$arena.appendChild($divPlayer2);
+const $button = document.querySelector('.button');
+const $divLifePlayer1 = document.querySelector('.player1 .life')
+const $divLifePlayer2 = document.querySelector('.player2 .life')
+const mapOfPlayer = new Map();
+mapOfPlayer.set(player1, $divLifePlayer1);
+mapOfPlayer.set(player2, $divLifePlayer2);
+const mapOfOpposite = new Map();
+mapOfOpposite.set(player1, player2);
+mapOfOpposite.set(player2, player1);
+const mapOfDamage = new Map();
+mapOfDamage.set('brain', 20);
+mapOfDamage.set('arrow', 15);
+mapOfDamage.set('knife', 10);
+mapOfDamage.set('pick', 22);
+mapOfDamage.set('gun', 25);
+
+
+$button.addEventListener('click', function () {
+    mapOfPlayer.forEach((div, player) => {
+        player.attack();
+        let opponent = mapOfOpposite.get(player);
+        let opponentLife = mapOfPlayer.get(opponent);
+        opponentLife.style.width = opponent.hp + '%';
+        if(opponent.hp === 0){
+            $arena.appendChild(playerWin(player));
+            $button.disabled = true;
+        }
+    })
+});
